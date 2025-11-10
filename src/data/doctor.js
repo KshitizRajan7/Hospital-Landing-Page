2f64790f296231aecfc799fd02541b53892717bf
kshitizrajan7 <kshitizrajandev@gmail.com>
2025-11-10 07:29:49 +0545

    feat(data): add dummy data for doctors and users

---
+++ src/data/doctor.js
@@ -0,0 +1,7 @@
+export const doctors = [
+  { id: 1, name: "Dr. Rajan Sharma", department: "Cardiology", specialization: "Heart Specialist" },
+  { id: 2, name: "Dr. Anisha Thapa", department: "Neurology", specialization: "Brain Specialist" },
+  { id: 3, name: "Dr. Suman Koirala", department: "Orthopedics", specialization: "Bone Specialist" },
+  { id: 4, name: "Dr. Nisha Gurung", department: "Pediatrics", specialization: "Child Specialist" },
+  { id: 5, name: "Dr. Bipin Rai", department: "Dermatology", specialization: "Skin Specialist" },
+];
